c119b3f87dfc5626b247cb02d08ca2f096542355
TsoiEn <paulanrenz23@gmail.com>
2024-12-17 22:41:19 +0800

    main

---
+++ backend/mappings.py
@@ -0,0 +1,9 @@
+# mappings.py
+
+GENDER_MAP = {"Male": 1, "Female": 2}
+FEVER_MAP = {"Yes": 1, "No": 0}
+COUGH_MAP = {"Yes": 1, "No": 0}
+FATIGUE_MAP = {"Yes": 1, "No": 0}
+DIFFICULTY_BREATHING_MAP = {"Yes": 1, "No": 0}
+BLOOD_PRESSURE_MAP = {"Low": 1, "Normal": 2, "High": 3}
+CHOLESTEROL_LEVEL_MAP = {"Low": 1, "Normal": 2, "High": 3}
